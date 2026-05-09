b302543ad31c0333c3caed8d69615e7f9de10410
Dirceu Neto <netinhoalves_2011@hotmail.com>
2026-05-09 05:33:45 -0400

    feat: cria PageWrapper e estrutura inicial de páginas

---
+++ frontend/src/pages/Inventario/Inventario.jsx
@@ -0,0 +1,14 @@
+import { PageWrapper } from '../../components/layout/PageWrapper';
+
+function Inventario() {
+  return (
+    <PageWrapper
+      title="Inventário"
+      description="Gerencie os produtos cadastrados"
+    >
+      <div />
+    </PageWrapper>
+  );
+}
+
+export default Inventario;
---
+++ frontend/src/pages/Movimentacoes/index.js
@@ -0,0 +1,1 @@
+export { default as Movimentacoes } from './Movimentacoes.jsx';
