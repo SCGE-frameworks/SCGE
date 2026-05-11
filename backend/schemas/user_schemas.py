48ce25148a4e93120ea18d6e3ae8457941a23cfd
Caiokl3i <caio.victor.santos12@gmail.com>
2026-05-11 18:54:09 -0400

    refactor: melhora schemas para usuário

--- backend/schemas/user_schemas.py
+++ backend/schemas/user_schemas.py
@@ -1,13 +1,15 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
 
 class UserCreate(BaseModel):
-    nome: str
-    email: str
-    senha: str
+    nome: str = Field(min_length=3, max_length=100)
+    email: EmailStr
+    senha: str = Field(min_length=8)
+
 
 class UserResponse(BaseModel):
     id: int
     nome: str
-    email: str
+    email: EmailStr
 
     model_config = ConfigDict(from_attributes=True)
