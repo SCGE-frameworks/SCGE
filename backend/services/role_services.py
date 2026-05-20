1055e4069b9c58d886d55547ef01896abd24a680
rhudsu <rhudus@gmail.com>
2026-05-19 23:20:02 -0400

    refactor: alterando logica de funcionamento require_role, esperando current_user agora

--- backend/services/role_services.py
+++ backend/services/role_services.py
@@ -1,22 +1,26 @@
 from sqlalchemy.orm import Session
+from backend.database import get_db
 from models.user import User
-from models.cargo import Cargos
+from models.cargo import Cargo
 from utils.responses import error_message, success_message
 
-def require_role(user_id, cargo, db: Session):
-    users = db.query(User).filter(User.id == user_id).first()
-    cargo = db.query(Cargos).filter(Cargos.nome.like(f"%{cargo}%")).first()
+def require_role(current_user: User = None, cargo: str = None, db: Session = None):
+    
+    if cargo is None:
+        return error_message("É preciso especificar um cargo", code="ROLE_NOT_FOUND", status_code=404)
 
-    if not users:
+    cargo = db.query(Cargo).filter(Cargo.nome.like(f"%{cargo}%")).first()
+
+    if not current_user or not current_user.ativo:
         return error_message("Nenhum usuário encontrado", code="USERS_NOT_FOUND", status_code=404)
 
     if cargo is None:
         return error_message("Cargo não encontrado", code="ROLE_NOT_FOUND", status_code=404)
 
-    if(not users[0].cargo_id):
+    if(not current_user.cargo_id):
         return error_message("O usuário não tem um cargo atribuído", code="ROLE_NOT_FOUND", status_code=404)
 
-    if(users[0].cargo_id == cargo.id):
+    if(current_user.cargo_id == cargo.id):
         return error_message("O usuário não tem permissão para acessar esta funcionalidade", code="FORBIDDEN", status_code=403)
 
-    return success_message("Usuário tem permissão para acessar esta funcionalidade", data={"user_id": users[0].id, "cargo": cargo.name}, db=db)
+    return success_message("Usuário tem permissão para acessar esta funcionalidade", data={"user_id": current_user.id, "cargo": cargo.name}, db=db)
