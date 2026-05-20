774fb9d7d6317a9d1a03a6ba75a7b707d6bffc23
rhudsu <rhudus@gmail.com>
2026-05-19 22:15:11 -0400

    fix: alteração do nome da tabela cargo => cargos

--- backend/models/cargo.py
+++ backend/models/cargo.py
@@ -2,6 +2,6 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 class Cargo(Base):
-    __tablename__ = "cargo"
+    __tablename__ = "cargos"
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String)
