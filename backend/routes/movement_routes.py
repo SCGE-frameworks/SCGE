23d5c782bf2a57c5fcfcf262042fdb9a473e0e10
Caiokl3i <caio.victor.santos12@gmail.com>
2026-06-15 16:24:18 -0400

    refactor: atualiza rotas com novos services de movimentações

--- backend/routes/movement_routes.py
+++ backend/routes/movement_routes.py
@@ -1,24 +1,28 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from backend.services.moviment_service import create_entry_service
+from backend.services.moviment_service import create_entry_service, create_exit_service, create_loss_service, get_movement_by_id_service, list_movements_service
 from database import get_db
 from schemas import MovimentCreate
 
 router = APIRouter(prefix="/movements", tags=["Stocks Movements"])
 
 @router.get("/")
-def list_movements():
-    return {"message": "Lista de movimentações de estoque"}
+def list_movements(db: Session = Depends(get_db)):
+    return list_movements_service(db)
+
+@router.get("/{movement_id}")
+def get_movement_by_id(movement_id: int, db: Session = Depends(get_db)):
+    return get_movement_by_id_service(movement_id, db)
 
 @router.post("/create_entry")
 def create_entry(entry: MovimentCreate, db: Session = Depends(get_db)):
     return create_entry_service(entry, db)
 
 @router.post("/create_exit")
-def register_exit():
-    return {"message": "Saída de estoque cadastrada com sucesso!"}
+def create_exit(exit: MovimentCreate, db: Session = Depends(get_db)):
+    return create_exit_service(exit, db)
 
-@router.get("/{movement_id}")
-def get_movement_details(movement_id: int):
-    return {"message": f"Detalhes da movimentação de estoque {movement_id}"}
+@router.post("/create_loss")
+def create_loss(loss: MovimentCreate, db: Session = Depends(get_db)):
+    return create_loss_service(loss, db)
